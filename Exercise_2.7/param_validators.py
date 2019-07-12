7c47caa59fef9d6fefdab6e4c63ffc483ef7b73f
Tal <Tal@TAL-COMPUTER>
2019-07-12 16:49:33 +0300

    fixed validate_COPY to work as needed

--- Exercise_2.7/param_validators.py
+++ Exercise_2.7/param_validators.py
@@ -36,17 +36,18 @@ def validate_DELETE(request_list):  # DELETE gets the name of the file to remove
 
 
 def validate_COPY(request_list):  # COPY gets the files to be copied
-    try:  # user can enter one param
-        if request_list[1] != None and request_list[2] != None:
+    if request_list[1] != None:
+        if len(request_list) > 2:  # user can enter one param
             param2 = os.getcwd() + '\\' + request_list[2]
+            print(f'request_list[1] = {request_list[1]} \nparam2 = {param2}')
             if os.path.isfile(request_list[1]) and os.path.isfile(param2):
                 return True
             else:
                 return [False, 'one of the files does not exist!']
         else:
-            return [False, 'Please add the files to be copied']
-    except IndexError:
-        return [False, 'Please add the second parameter']
+            return [False, 'Please add the second parameter']
+    else:
+        return [False, 'Please add the filed to be copied']
 
 
 def validate_EXECUTE(request_list):  # EXECUTE gets the file to execute
