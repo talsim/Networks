50a6f384b7149b92c29bcb5268d1fb9add2685be
Tal <Tal@TAL-COMPUTER>
2019-07-12 16:52:46 +0300

    improved check_client_request by removing unnecessary lines

--- Exercise_2.7/server.py
+++ Exercise_2.7/server.py
@@ -1,13 +1,15 @@
 import socket
-from PIL import ImageGrab
 import os.path
 import param_validators
+import handle_client_request_impl
 
 
 IP = '0.0.0.0'
 PORT = 8820
-#request_list[0] = command
-#request_list[1 ++] = params
+
+
+# request_list[0] = command
+# request_list[1 ++] = params
 
 
 def receive_client_request(client_socket):
@@ -21,13 +23,13 @@ def receive_client_request(client_socket):
 
     Example: 12DIR c:\cyber as input will result in command = 'DIR', params = 'c:\cyber'
     """
-    recvsize = client_socket.recv(2) # length of the request
-    request = client_socket.recv(int(recvsize.decode())) # the request
-    return my_split(request.decode()) # splitting the request to command and params
-
+    recvsize = client_socket.recv(2)  # length of the request
+    request = client_socket.recv(int(recvsize.decode()))  # the request
+    return my_split(request.decode())  # splitting the request to command and params
 
 
 def check_client_request(request_list):
+
     """Check if the params are good.
     For example, the filename to be copied actually exists
     Returns:
@@ -36,17 +38,14 @@ def check_client_request(request_list):
     """
 
     command = request_list[0]
+    print(f'request_list = {request_list}')
     if request_list[1] != None:
         request_list[1] = os.getcwd() + '\\' + request_list[1]
-    else:
-        request_list[1] = None
-    print(f'request_list = {request_list}')
-    if command == 'TAKE_SCREENSHOT':
+    if command == 'TAKE_SCREENSHOT' or command == 'EXIT':
         return True
-    elif command == 'EXIT':
-        return True
-    return getattr(param_validators, f'validate_{command}')(request_list)
-
+    validator = getattr(param_validators, f'validate_{command}')(request_list)
+    print(f'validator = {validator}')
+    return validator
 
 def handle_client_request(request_list, c_socket):
     """Create the response to the client, given the command is legal and params are OK
@@ -57,7 +56,7 @@ def handle_client_request(request_list, c_socket):
         response: the requested data
     """
     command = request_list[0]
-    import handle_client_request_impl
+
     if command == 'EXIT':
         return 'Quiting'
     elif command == 'COPY':
@@ -71,15 +70,15 @@ def handle_client_request(request_list, c_socket):
     data = getattr(handle_client_request_impl, command)(request_list[1])
     print(f'data = {data}')
     return data
-	
 
-def send_response_to_client(response, client_socket, command = None):
+
+def send_response_to_client(response, client_socket, command=None):
     """Create a protocol which sends the response to the client
 
     The protocol should be able to handle short responses as well as files
     (for example when needed to send the screenshot to the client)
     """
-    if command is None: # if respone = error_msg
+    if command is None:  # if respone = error_msg
         client_socket.send(str(response).encode())
     else:
         client_socket.send(str(response).encode())
@@ -93,8 +92,8 @@ def my_split(string):
     return list_string
 
 
-#validation_list[0] = valid (True/False)
-#validation_list[1] = error_msg (if valid is False)
+# validation_list[0] = valid (True/False)
+# validation_list[1] = error_msg (if valid is False)
 def main():
     # open socket with client
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -106,7 +105,7 @@ def main():
     while not done:
         request_list = receive_client_request(client_socket)
         validation_list = check_client_request(request_list)
-        if validation_list == True: # if valid is True
+        if validation_list == True:  # if valid is True
             response = handle_client_request(request_list, client_socket)
             send_response_to_client(response, client_socket, request_list[0])
         else:
