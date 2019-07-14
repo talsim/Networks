89e14f8df12f6c4143115d499a1f7c4e068837ae
Tal <Tal@TAL-COMPUTER>
2019-07-14 18:53:21 +0300

    implemented TAKE_SCREENSHOT

--- Exercise_2.7/handle_client_request_impl.py
+++ Exercise_2.7/handle_client_request_impl.py
@@ -1,30 +1,32 @@
 from os import listdir
-
+from PIL import ImageGrab
 
 # request_list[0] = command
 # request_list[1 ++] = params
 
 
-def DIR(param):
-    files_list = listdir(param)
+def DIR(request_list):
+    files_list = listdir(request_list[1])
     return files_list
 
-def SEND_FILE(param):
-    f = open(param, 'rb')
+def SEND_FILE(request_list):
+    f = open(request_list[1], 'rb')
     return f
 
-def TAKE_SCREENSHOT(param):
+def TAKE_SCREENSHOT(request_list):
+    im = ImageGrab.grab()
+    im.save('screenshot_to_send.png')
+    return im
+
+def DELETE(request_list):
     pass
 
-def DELETE(param):
+
+def COPY(request_list):
     pass
 
 
-def COPY(param1, param2):
-    pass
-
-
-def EXECUTE(param):
+def EXECUTE(request_list):
    pass
 
 
