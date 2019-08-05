2f6682b0371e0ab30c7242c111ce5aa1948e447d
Tal <Tal@TAL-COMPUTER>
2019-08-05 13:26:51 +0300

    fixed an issue in calculate_area and added is_valid_string

--- Exercise_4.4-4.11/WebFunctions.py
+++ Exercise_4.4-4.11/WebFunctions.py
@@ -1,25 +1,32 @@
-
 def calculate_next(param):
-    for char in str(param):
-        if char.isalpha(): # if client entered a letter, return error
-            return 'Error: Please enter a number'
-    n_param = int(param) + 1
-    return str(n_param)
+    if is_valid_string(str(param)):
+        n_param = int(param) + 1
+        return str(n_param)
+    return 'Error: Please enter a number'
+
 
 
 def calculate_area(params):
-    params = str(params)
-    #Parser to get height and width:
-    height = params.split('=', 1)[1]
-    height = height[:1]
-    params = params.split('&', 1)[1]
-    width = params.split('=', 1)[1]
-    for c in width:
-        if c.isalpha():
-            return 'Error: Please enter valid numbers'
-    for c in height:
-        if c.isalpha():
-            return 'Error: Please enter valid numbers'
+    width, height = calc_area_parser(params)
+    if is_valid_string(width) and is_valid_string(height):
+        return str((int(width) * int(height)) / 2)
+    return 'Error: Please enter valid numbers'
 
-    return str((int(width) * int(height))/2)
 
+def upload(param):
+    pass
+
+
+def calc_area_parser(param):
+    param = str(param)
+    height = param.split('=', 1)[1].split('&', 1)[0]
+    param = param.split('&', 1)[1]
+    width = param.split('=', 1)[1]
+    return [width, height]
+
+
+def is_valid_string(s):
+    for c in s:
+        if c.isalpha():
+            return False
+    return True
