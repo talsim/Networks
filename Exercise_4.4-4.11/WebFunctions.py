066555b6b0d708adedb95b4dc17e88185438f6ce
Tal <Tal@TAL-COMPUTER>
2019-08-06 19:05:49 +0300

    implemented upload function for uploading files to the server

--- Exercise_4.4-4.11/WebFunctions.py
+++ Exercise_4.4-4.11/WebFunctions.py
@@ -1,9 +1,11 @@
+import os
+
+
 def calculate_next(param):
     if is_valid_string(str(param)):
         n_param = int(param) + 1
         return str(n_param)
     return 'Error: Please enter a number'
-
 
 
 def calculate_area(params):
@@ -13,8 +15,23 @@ def calculate_area(params):
     return 'Error: Please enter valid numbers'
 
 
-def upload(param):
-    pass
+def upload(file_name, client, req):
+    size = req.split(b'Content-Length: ')[1].split(b'\r\n')[0]  # getting the size of the image
+    rest_data = req.split(b'\r\n\r\n')[1]  # rest of data after the header
+    chunk = 20000  # chunk to recv every time
+    data = rest_data + client.recv(chunk)
+    total_recv = len(data)
+    upload_path = f'{os.getcwd()}{os.sep}webroot{os.sep}uploads{os.sep}{file_name}'
+    with open(upload_path, 'wb') as f:
+        f.write(data)
+        while total_recv < int(size):
+            data = client.recv(chunk)
+            print('receiving')
+            total_recv += len(data)
+            f.write(data)
+            print('writing')
+        print('Download Complete')
+    return 'File has uploaded successfully!'
 
 
 def calc_area_parser(param):
--- Exercise_4.4-4.11/WebServer.py
+++ Exercise_4.4-4.11/WebServer.py
@@ -15,30 +15,30 @@ def get_client_request(client):
     Get the client request and spilt it to command(GET) and file path to serve.
     Return the file path and the method
     """
-    req = client.recv(1024).decode()
-    logging.info(f'request = {req}')
-    if req == '':
-        return [None, '']
-    string_list = req.split(' ')  # Split request from spaces
+    req = client.recv(1024)
+    if req == b'':
+        return None
+
+    req_content = req.split(b'\r\n\r\n')[0].decode()
+    string_list = req_content.split(' ')  # Split request from spaces
     method = string_list[0]  # First string is the method
     addr = string_list[1]  # Second string is the address
-    logging.info(f'address = {addr}')
     if '?' not in addr:  # GET doesn't have any parameters (means addr is a file)
         current_dir = Path.cwd()
-        fileName = addr
-        if fileName == '/':
-            filePath = current_dir / 'webroot' / 'index.html'  # Load index file as root
+        file_name = addr
+        if file_name == '/':
+            file_path = current_dir / 'webroot' / 'index.html'  # Load index file as root
         else:
-            fileName = fileName.replace("/", os.sep)  # replacing all the separators with the ones similar to the OS
-            filePath = f'{current_dir}{os.sep}webroot{fileName}'
-        return [filePath, method]
+            file_name = file_name.replace("/", os.sep)  # replacing all the separators with the ones similar to the OS
+            file_path = f'{current_dir}{os.sep}webroot{file_name}'
+        return [file_path, method]
     func = addr.split('?')[0].split('/')[1].replace('-', '_')
     logging.info(f'func = {func}')
     params = addr.split('?', 1)[1]  # GET parameters
     if '&' not in params:  # only one parameter
         params = params.split('=')[1]
     logging.info(f'params = {params}')
-    return [addr, method, func, params]
+    return [addr, method, func, params, req]
 
 
 def check_given_method(method):
@@ -50,34 +50,34 @@ def check_given_method(method):
     return False
 
 
-def get_mimetype(filePath):
+def get_mimetype(file_path):
     """
     Get mimetype for generating the headers.
 
     Parameters:
-        filePath - the file path.
+        file_path - the file path.
     Returns:
         mimetype - the correct mimetype to generate the headers with.
     """
-    filePath = str(filePath)
-    if filePath.endswith(('.txt', '.html')):
+    file_path = str(file_path)
+    if file_path.endswith(('.txt', '.html')):
         mimetype = 'text/html; charset=utf-8'
-    elif filePath.endswith('.jpg'):
+    elif file_path.endswith('.jpg'):
         mimetype = 'image/jpg'
-    elif filePath.endswith('.js'):
+    elif file_path.endswith('.js'):
         mimetype = 'text/javascript; charset=UTF-8'
-    elif filePath.endswith('.css'):
+    elif file_path.endswith('.css'):
         mimetype = 'text/css'
-    elif filePath.endswith('.ico'):
+    elif file_path.endswith('.ico'):
         mimetype = 'image/x-icon'
-    elif filePath.endswith('.gif'):
+    elif file_path.endswith('.gif'):
         mimetype = 'image/gif'
     else:
         mimetype = 'text/plain'
     return mimetype
 
 
-def generate_headers(status_code, filePath=None, data=None):
+def generate_headers(status_code, file_path=None, data=None):
     """
     Generates the headers for http response
     with the status_code given
@@ -86,30 +86,34 @@ def generate_headers(status_code, filePath=None, data=None):
     if status_code == HTTPStatus.OK.value:
         header += 'HTTP/1.1 200 OK\n'
         if data is not None:
-            header += f'Content-Length: {len(str(data))}\n'
-            header += 'Content-Type: text/plain\n'
-        elif filePath is not None:
-            header += f'Content-Length: {os.path.getsize(filePath)}\n'
-            header += f'Content-Type: {get_mimetype(filePath)}\n'
+            header += f'Content-Length: {len(str(data))}\r\n'
+            header += 'Content-Type: text/plain\r\n'
+        elif file_path is not None:
+            header += f'Content-Length: {os.path.getsize(file_path)}\r\n'
+            header += f'Content-Type: {get_mimetype(file_path)}\r\n'
     elif status_code == HTTPStatus.NOT_FOUND.value:
-        header += 'HTTP/1.1 404 Not Found\n'
-    header += f'Date: {datetime.now()}\n'
-    header += 'Server: Gvahim Http WebServer\n\n'
+        header += 'HTTP/1.1 404 Not Found\r\n'
+    header += f'Date: {datetime.now()}\r\n'
+    header += 'Server: Gvahim Http WebServer\r\n\r\n'
     logging.info(f'\nheader = {header}')
     return header
 
 
-def handle_client(status_code, client, func=None, filePath_to_serve=None, params=None):
+def handle_client(status_code, client, func=None, file_path_to_serve=None, params=None, req=None):
     """
     Main function for handling connected clients and serving files from webroot
     """
     if status_code == HTTPStatus.OK.value:  # 200
-        if filePath_to_serve is not None:
-            with open(filePath_to_serve, 'rb') as file:
+        if file_path_to_serve is not None:
+            with open(file_path_to_serve, 'rb') as file:
                 response_data = file.read()
-            header = generate_headers(status_code, filePath_to_serve)
+            header = generate_headers(status_code, file_path_to_serve)
         elif params is not None:
-            response_data = getattr(WebFunctions, func)(params)
+            if func == 'upload':
+                # params = f'<file_name>{params}</file_name> <client>{client}</client> <request>{req}</request>' # adding additional parameters that upload function requires
+                response_data = getattr(WebFunctions, func)(params, client, req)
+            else:
+                response_data = getattr(WebFunctions, func)(params)
             header = generate_headers(status_code, data=response_data)
             response_data = str(response_data).encode()
             logging.info(f'DATA: {response_data}')
@@ -141,16 +145,16 @@ def main():
         client_socket, address = server_socket.accept()
         logging.info(f'Client connected from: {address}')
         req_list = get_client_request(client_socket)
-        if req_list[1] == '':
+        if req_list == None:
             logging.info('Client disconnected')
             continue
         logging.info(f'address: {req_list[0]}\nmethod: {req_list[1]}')
         valid_method = check_given_method(req_list[1])
         if valid_method:
             if len(req_list) > 2:  # if there is parameters, pass it to handle_client()
-                handle_client(200, client_socket, func=req_list[2], params=req_list[3])
+                handle_client(200, client_socket, func=req_list[2], params=req_list[3], req=req_list[4])
             elif os.path.isfile(req_list[0]):  # if file exist
-                handle_client(200, client_socket, filePath_to_serve=req_list[0])
+                handle_client(200, client_socket, file_path_to_serve=req_list[0])
             else:  # file doesn't exist
                 handle_client(404, client_socket)
         else:  # method not valid (only GET method supported)
