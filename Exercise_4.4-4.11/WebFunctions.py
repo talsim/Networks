2ee9d98332e7b6d986b9f5b445be0b58d24aa7f2
Tal <Tal@TAL-COMPUTER>
2019-08-07 15:31:28 +0300

    Implemented image function for sending image to client and added build_path for building the file path

--- Exercise_4.4-4.11/WebFunctions.py
+++ Exercise_4.4-4.11/WebFunctions.py
@@ -1,28 +1,34 @@
 import os
+from pathlib import Path
 
 
 def calculate_next(param):
     if is_valid_string(str(param)):
+        param = param[0]
         n_param = int(param) + 1
         return str(n_param)
     return 'Error: Please enter a number'
 
 
 def calculate_area(params):
+    params = params[0]
     width, height = calc_area_parser(params)
     if is_valid_string(width) and is_valid_string(height):
         return str((int(width) * int(height)) / 2)
     return 'Error: Please enter valid numbers'
 
 
-def upload(file_name, client, req):
+def upload(params_list):
+    name = params_list[0]
+    client = params_list[1]
+    req = params_list[2]
     size = req.split(b'Content-Length: ')[1].split(b'\r\n')[0]  # getting the size of the image
     rest_data = req.split(b'\r\n\r\n')[1]  # rest of data after the header
     chunk = 20000  # chunk to recv every time
     data = rest_data + client.recv(chunk)
     total_recv = len(data)
-    upload_path = f'{os.getcwd()}{os.sep}webroot{os.sep}uploads{os.sep}{file_name}'
-    with open(upload_path, 'wb') as f:
+    path = build_path('uploads', name)
+    with open(path, 'wb') as f:
         f.write(data)
         while total_recv < int(size):
             data = client.recv(chunk)
@@ -34,11 +40,21 @@ def upload(file_name, client, req):
     return 'File has uploaded successfully!'
 
 
+def image(params_list):
+    name = params_list[0]
+    client = params_list[1]
+    path = build_path('uploads', name)
+    if os.path.isfile(path):
+        img = open(path, 'rb')
+        data = img.read()
+        client.sendall(data)
+    return 'Error: The file does not exist'
+
+
 def calc_area_parser(param):
     param = str(param)
-    height = param.split('=', 1)[1].split('&', 1)[0]
-    param = param.split('&', 1)[1]
-    width = param.split('=', 1)[1]
+    height = param.split('height=', 1)[1].split('&width', 1)[0]
+    width = param.split('width=', 1)[1]
     return [width, height]
 
 
@@ -47,3 +63,15 @@ def is_valid_string(s):
         if c.isalpha():
             return False
     return True
+
+
+def build_path(folder, name):
+    """
+    Function for building path to root
+    """
+    folder = Path(folder)
+    name = Path(name)
+    root = f'{os.getcwd()}{os.sep}webroot'
+    directory = folder / name
+    full_path = root / directory
+    return full_path
